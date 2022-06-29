cc6c1539f6b2f5085c9377fe30333e7f72107c3a
burak baki erdogan <bbe2007@outlook.com>
2022-06-29 23:58:36 +0300

    done

--- inter.c
+++ inter.c
@@ -1,19 +1,14 @@
 /*
-Assignment name  : inter
-Expected files   : inter.c
-Allowed functions: write
---------------------------------------------------------------------------------
-
-Write a program that takes two strings and displays, without doubles, the
-characters that appear in both strings, in the order they appear in the first
-one.
-
-The display will be followed by a \n.
-
-If the number of arguments is not 2, the program displays \n.
-
+   Assignment name  : inter
+   Expected files   : inter.c
+   Allowed functions: write
+   --------------------------------------------------------------------------------
+   Write a program that takes two strings and displays, without doubles, the
+   characters that appear in both strings, in the order they appear in the first
+   one.
+   The display will be followed by a \n.
+   If the number of arguments is not 2, the program displays \n.
 Examples:
-
 $>./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
 padinto$
 $>./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
@@ -25,42 +20,24 @@ $
 */
 
 #include <unistd.h>
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
 
-int	ft_strlen(char *s)
+int	ft_strlen(char *s1)
 {
-	int	index;
+	int	index = 0;
 
-	index = 0;
-	while (s[index] != '\0')
+	while (s1[index] != '\0')
 		index++;
 	return (index);
-}
-void	ft_strrev(char *s1, int size)
-{
-	char	temp[size];
-	int		total;
-
-
-	int index = 0;
-	total = size;
-	while (size >= 0)
-	{
-		temp[index] = s1[size];
-		index++;
-		size--;
-	}
-	index = 0;
-	while (index < total + 1 )
-	{
-		s1[index] = temp[index];
-		index++;
-	}
 }
 
 int	ft_compare(char *s1, char s2, int size)
 {
-	int	index;
-	int	total;
+	int index;
+	int total;
+	int index2;
 
 	index = 0;
 	total = 0;
@@ -71,42 +48,59 @@ int	ft_compare(char *s1, char s2, int size)
 		index++;
 	}
 	return (total);
+
 }
 
-int	main(int ac, char *argv[])
+void ft_rev(char *s1, int size)
 {
-	char	str[ft_strlen(argv[1]) + ft_strlen(argv[2])];
-	int		index;
-	int		index2;
-	int		total;
+	char temp[size];
+	int index = 0;
+	int total = size;
+	while (index <= total)
+	{
+		temp[index] = s1[size];
+		index++;
+		size--;
+	}
+	index = 0;
+	while (index <= total)
+	{
+		s1[index] = temp[index];
+		index++;
+	}
+}
+int main (int ac, char **argv)
+{
+	char	str[ft_strlen(argv[1])];
+	int len;
+	int index;
+	int index2 = 0;
 
-	index = ft_strlen(argv[1]);
-	index2 = 0;
-	total = 0;
-
-
+	index = 0;
+	len = ft_strlen(argv[1]);
 	if (ac == 3)
 	{
-		while (index >= 0)
+		while (len >= 0)
 		{
-			if (ft_compare(argv[1], argv[1][index], index) == 0)
-				{
-					str[index2] = argv[1][index];
-					index2++;
-					total++;
-				}
+			if (ft_compare(argv[1], argv[1][len], len) == 0)
+			{
+				str[index] =  argv[1][len];
+				index++;
+			}
+			len--;
+		}
+		ft_rev(str, (index - 1) );
+
+		int size = ft_strlen(argv[2]);
+		while(index)
+		{
+			if (ft_compare(argv[2], str[index2], size))
+				write(1, &str[index2], 1);
+			index2++;
 			index--;
 		}
-		ft_strrev(str, total);
-		index = 0;
-		int k = ft_strlen(argv[2]);
-		while (total)
-		{
-			if (ft_compare(argv[2], str[index], k))
-				write(1, &str[index], 1);
-			index++;
-			total--;
-		}
+		write(1, "\n", 1);
 	}
+	else
 	write(1, "\n", 1);
 }
---
+++ inter_grademe.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+gcc -o inter inter.c
+​
+./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+./inter "rien" "cette phrase ne cache rien" | cat -e
+./inter | cat -e
