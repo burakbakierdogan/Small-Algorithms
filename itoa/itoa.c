d83718ce8956382de110c124eccbc1f802a9d46e
burak baki erdogan <bbe2007@outlook.com>
2022-07-16 14:26:14 +0300

    done

---
+++ itoa/itoa.c
@@ -0,0 +1,78 @@
+/*
+Assignment name  : ft_itoa
+Expected files   : ft_itoa.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write a function that takes an int and converts it to a null-terminated string.
+The function returns the result in a char array that you must allocate.
+
+Your function must be declared as follows:
+
+char	*ft_itoa(int nbr);
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int	ft_len(int nbr)
+{
+	int	index;
+	int	number;
+
+	index = 0;
+	if (nbr < 0)
+	{
+		number = nbr * -1;
+		index++;
+	}
+	number = nbr;
+	while (number)
+	{
+		number /= 10;
+		index++;
+	}
+	return (index);
+}
+
+
+char	*ft_putnbr(char *str, int len, int number)
+{
+	while (number && len >= 0)
+	{
+		str[len--] = 48 + (number % 10);
+		number /= 10;
+	}
+	return (str);
+}
+
+char	*ft_itoa(int nbr)
+{
+	char	*str;
+	int		len;
+	int		index;
+	int		number;
+
+	len = ft_len(nbr);
+	number = nbr;
+	if (nbr == -2147483648)
+		return ("-2147483648");
+	if (nbr == 0)
+		return ("0");
+	if (nbr < 0)
+		number = nbr * -1;
+	str = (char *) malloc (len * sizeof(char));
+	str[len] = '\0';
+	len--;
+	str = ft_putnbr(str, len, number);
+	if (nbr < 0)
+		str[0] = '-';
+	return (str);
+}
+
+int main (void) // for test purpose
+{
+	int a = INT_MAX;
+	printf("%s\n", ft_itoa(-1121233312));
+}
