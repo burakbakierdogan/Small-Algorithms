5438cf02e26f8fe04cf027427f0f9e789bf0b2d8
burak baki erdogan <bbe2007@outlook.com>
2022-07-16 14:29:36 +0300

    deleted

--- aff_a/grademe.sh
+++
@@ -1,6 +0,0 @@
-clang -o aff_a aff_a.c -Wall -Werror -Wextra
-echo "$(./aff_a "abc" | cat -e)"
-echo "$(./aff_a "dubO a POIL" | cat -e)"
-echo "$(./aff_a "zz sent le poney" | cat -e)"
-echo "$(./aff_a | cat -e)"
-
--- aff_first_param/grademe.sh
+++
@@ -1,4 +0,0 @@
-clang -o aff_first_param aff_first_param.c -Wall -Werror -Wextra
-echo "$(./aff_first_param vincent mit "l'ane" dans un pre et "s'en" vint | cat -e)"
-echo "$(./aff_first_param "j'aime le fromage de chevre" | cat -e)"
-echo "$(./aff_first_param)"
